58d33fbcb8ac9d8ccadef8caeef707171112e882
Ezziger <84440513+Ezziger@users.noreply.github.com>
2021-07-19 17:03:18 +0200

    mise en place du header du footer sur la page index.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<!--**********************************CREATION DES SEJOURS**********************************-->
+
+<!--**********************************SEJOUR EN GRECE**********************************-->
+<?php
+
+$grece = [
+        "nom_du_sejour" => "A l'abordage de Santorin",
+        "small_description" => "Soleil, détente et bord de mer",
+        "long_description" => "elle sera longue plus tard",
+        "durée" => "7 jours et 6 nuits",
+        "formule" => "demi-pension",
+        "prix" => "600"
+    ]
+
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include "header.php"
+?>
+
+<h1>C'est ici que vont apparaitre mes offres de voyages</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include "footer.php"
+?>
